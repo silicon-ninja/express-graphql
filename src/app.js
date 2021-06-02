255a1a4f4607b1d03d8b601200ed39e98009d4e5
Srikanth Kandarp <48326585+Srikanth-Kandarp@users.noreply.github.com>
2021-06-02 14:00:40 +0530

    added mongo connection

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
+import 'dotenv';
 
 const app = express();
 
 app.use(bodyParser.json());
-
+mongoose.connect(process.env.MongoURL, { useNewUrlParser: true });
 app.get('/', (req, res, next) => {
   res.send('Hello this is working');
 });
 
-let Port = 8080;
+let Port = 8080 || process.env.PORT;
 app.listen(`${Port}`, () => {
   console.log(`Listening to Port : ${Port}`);
 });
