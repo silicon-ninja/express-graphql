3c8669a34c8ff02bc291fa439d2b1f8bc9b91194
Srikanth Kandarp <48326585+Srikanth-Kandarp@users.noreply.github.com>
2021-06-03 23:38:50 +0530

    basic graphql queries

--- src/app.js
+++ src/app.js
@@ -1,15 +1,63 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
-import 'dotenv';
-
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const { buildSchema } = require('graphql');
+require('dotenv').config();
+const mongoose = require('mongoose');
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
-mongoose.connect(process.env.MongoURL, { useNewUrlParser: true });
-app.get('/', (req, res, next) => {
-  res.send('Hello this is working');
-});
+// mongoose.connect(process.env.MongoURL, { useNewUrlParser: true });
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+    type Event {
+      _id:ID!,
+      Event_Name:String!
+      Event_Description:String!
+      Event_Price:Float!
+      Date:String!
+    }
+    type RootQuery{
+        events: [Event!]!
+    }
+
+    input EventInput{
+      Event_Name:String!
+      Event_Description:String!
+      Event_Price:Float!
+      Date:String!
+    }
+
+    type RootMutation{
+      createEvent(eventInput:EventInput):Event
+    }
+    schema{
+      query: RootQuery
+      mutation: RootMutation
+    }
+    `),
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: (args) => {
+        const event = {
+          _id: Math.random().toString,
+          Event_Name: args.Event_Name,
+          Event_Description: args.Event_Description,
+          Event_Price: +args.Event_Price,
+          Date: new Date().toISOString,
+        };
+        events.push(event);
+      },
+    },
+    graphiql: true,
+  })
+);
 
 let Port = 8080 || process.env.PORT;
 app.listen(`${Port}`, () => {
